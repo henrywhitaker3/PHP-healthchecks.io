d76c50e924ee83174dcc965544ffac218c6f6c37
Henry Whitaker <henrywhitaker3@outlook.com>
2020-08-10 23:22:55 +0100

    Removed tinker file
    Committed file with API key and UUID. If you're reading this and have looked through the commits to find this apikey, it has been revoked and UUID for the check deleted

--- tests/tinker.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Henrywhitaker3\Healthchecks\HealthchecksManager;
-
-require_once(__DIR__.'/../vendor/autoload.php');
-
-$hm = new HealthchecksManager('g2LEsTFpvrlQwrWjNPgwBjaVizLPP0sc');
-
-print_r($hm->getCheckStatusChanges('de680c32-60d3-4c95-9470-8f9859679a51'));
